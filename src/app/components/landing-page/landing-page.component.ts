1222e40502679733cd1a18edf7c40ac91c66ade2
Chneemann <Andre-Ke@gmx.de>
2024-03-28 15:03:18 +0100

    added components

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -2,11 +2,20 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { Router, RouterModule } from '@angular/router';
 import { UserService } from '../../service/user.service';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { MainChatComponent } from '../main-chat/main-chat.component';
+import { SecondaryChatComponent } from '../secondary-chat/secondary-chat.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    SidebarComponent,
+    MainChatComponent,
+    SecondaryChatComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
@@ -14,6 +23,10 @@ export class LandingPageComponent {
   constructor(public userService: UserService, private router: Router) {}
 
   ngOnInit() {
+    this.ifUserLogin();
+  }
+
+  ifUserLogin() {
     if (!this.userService.isUserLogin) {
       this.router.navigateByUrl('/login');
     }
