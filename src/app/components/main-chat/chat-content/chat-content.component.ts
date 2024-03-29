74be660422e07aa3544c8fe25cd618b8fd36a536
Chneemann <Andre-Ke@gmx.de>
2024-03-29 11:53:16 +0100

    design chat

--- src/app/components/main-chat/chat-content/chat-content.component.ts
+++ src/app/components/main-chat/chat-content/chat-content.component.ts
@@ -4,11 +4,17 @@ import { ChatService } from '../../../service/chat.service';
 import { UserService } from '../../../service/user.service';
 import { SingleChatComponent } from '../single-chat/single-chat.component';
 import { ChatMsgBoxComponent } from '../chat-msg-box/chat-msg-box.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-content',
   standalone: true,
-  imports: [MainChatComponent, SingleChatComponent, ChatMsgBoxComponent],
+  imports: [
+    MainChatComponent,
+    SingleChatComponent,
+    ChatMsgBoxComponent,
+    CommonModule,
+  ],
   templateUrl: './chat-content.component.html',
   styleUrl: './chat-content.component.scss',
 })
--- src/app/components/main-chat/single-chat/single-chat.component.ts
+++ src/app/components/main-chat/single-chat/single-chat.component.ts
@@ -2,17 +2,19 @@ import { Component, Input } from '@angular/core';
 import { User } from '../../../interface/user.interface';
 import { Chat } from '../../../interface/chat.interface';
 import { ChatContentComponent } from '../chat-content/chat-content.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-chat',
   standalone: true,
-  imports: [ChatContentComponent],
+  imports: [ChatContentComponent, CommonModule],
   templateUrl: './single-chat.component.html',
   styleUrl: './single-chat.component.scss',
 })
 export class SingleChatComponent {
   @Input() user!: User;
   @Input() chat!: Chat;
+  @Input() index!: number;
 
   convertTimestamp(timestamp: number) {
     var a = new Date(timestamp * 1000);
