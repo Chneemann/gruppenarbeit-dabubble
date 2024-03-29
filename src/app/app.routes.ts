ba8903c746d2fd690edc04deab0c6fd70929caca
HomeBjo <bjoernteneicken@gmail.com>
2024-03-29 09:33:55 +0100

    register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/login/register/register.component';
+import { ChooseAvatarComponent } from './components/login/choose-avatar/choose-avatar.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'avatar', component: ChooseAvatarComponent },
   { path: 'main', component: MainComponent },
   { path: 'main/:id', component: MainComponent },
 ];
--- src/app/components/login/register/register.component.ts
+++ src/app/components/login/register/register.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+ 
+  usersData = {
+    name:'',
+    email: '',
+    password: '',
+  };
 
+  constructor(){
+
+  }
+  onSubmit(form: NgForm) {
+    console.log('Registrierungsversuch mit:',this.usersData);
+    form.resetForm();
+  }
 }
