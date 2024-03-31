dc03686876d2e4d16ee69d0ba9d02f04243acc74
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-03-31 13:56:57 +0200

    added variable.name du header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { UserService } from '../../../service/user.service';
+import { User } from '../../../interface/user.interface';
 
 
 @Component({
@@ -15,19 +17,38 @@ export class HeaderComponent {
   showCurrentProfile = false;
   isOnline = true; //---- Zeige onlineStatus an
   closeProfil = false;
+  currentLoggtinUser = {};
+filteredUser: any;
 
 
-  constructor() {}
+  constructor(public userService: UserService) {}
+
 
   showSideMenu() {
     this.openMenu = !this.openMenu;
   }
 
+
   showProfile(){
     this.showCurrentProfile = true;
     this.closeProfil = false;
   }
+
+
   updateTestValue(value: boolean){
     this.showCurrentProfile = value;  
   }
+
+
+  getUsers(): User[] {
+    return this.userService.allUsers;
+  }
+
+
+  getCuurentUsers(userId: string) {
+    // userId = userId.replace(/\s/g, '');
+    const filteredUser = this.getUsers().filter((user) => user.id == userId);
+    return filteredUser;
+  }
+
 }
