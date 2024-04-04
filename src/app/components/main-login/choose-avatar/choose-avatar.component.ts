11808ad9106c9ae0cdafa822bb8bdcf8b8b75b9b
HomeBjo <bjoernteneicken@gmail.com>
2024-04-04 11:38:53 +0200

    fix register with header and footer

--- src/app/components/main-login/choose-avatar/choose-avatar.component.ts
+++ src/app/components/main-login/choose-avatar/choose-avatar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../../shared/components/login/header/header.component";
 import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
+import { RouterModule } from '@angular/router';
+import { SmallBtnComponent } from "../../../shared/components/small-btn/small-btn.component";
 
 @Component({
     selector: 'app-choose-avatar',
     standalone: true,
     templateUrl: './choose-avatar.component.html',
     styleUrl: './choose-avatar.component.scss',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent, RouterModule, SmallBtnComponent]
 })
 export class ChooseAvatarComponent {
 
