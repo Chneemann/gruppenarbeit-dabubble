bd24acf13d583a319afbee45b689267fd67b1064
HomeBjo <bjoernteneicken@gmail.com>
2024-04-04 08:36:34 +0200

    add shared/footer/header

--- src/app/components/main-login/choose-avatar/choose-avatar.component.ts
+++ src/app/components/main-login/choose-avatar/choose-avatar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../../shared/components/login/header/header.component";
+import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
 
 @Component({
-  selector: 'app-choose-avatar',
-  standalone: true,
-  imports: [],
-  templateUrl: './choose-avatar.component.html',
-  styleUrl: './choose-avatar.component.scss'
+    selector: 'app-choose-avatar',
+    standalone: true,
+    templateUrl: './choose-avatar.component.html',
+    styleUrl: './choose-avatar.component.scss',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class ChooseAvatarComponent {
 
