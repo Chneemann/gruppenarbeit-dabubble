df205e2e28a6750500ddc44db1221cf255abbb93
HomeBjo <bjoernteneicken@gmail.com>
2024-04-04 15:26:23 +0200

    add preview own avatar

--- src/app/components/main-login/choose-avatar/choose-avatar.component.ts
+++ src/app/components/main-login/choose-avatar/choose-avatar.component.ts
@@ -13,4 +13,29 @@ import { SmallBtnComponent } from "../../../shared/components/small-btn/small-bt
 })
 export class ChooseAvatarComponent {
 
+    avatarSrc: string = '/assets/img/charater1.svg';
+   
+    
+
+
+
+    onFileChange(event: any) {
+        if (event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
+      
+            // filereader um die Datei zu lesen
+            const reader = new FileReader();
+            reader.onload = (e: any) => {
+              this.avatarSrc = e.target.result; //das gelesene als bild bez avatar
+            };
+            reader.readAsDataURL(file); // lesen die datei als  URL
+          }
+        }
+      
+
+      showCurrentFile(file: File) {
+        const blob = new Blob([file], { type: file.type });  // Blob (Binary Large Object) 
+        const url = URL.createObjectURL(blob); // Erstelle einen Objekt-URL für den blon
+        window.open(url, '_blank'); // öffne den datei in einem neuen Fenster
+      }
 }
