dd4be489f189a45cc848dd7ac80e63f1249c0ecc
HomeBjo <bjoernteneicken@gmail.com>
2024-04-08 16:55:57 +0200

    add passwordResets Components

--- src/app/components/main-login/main-login.component.ts
+++ src/app/components/main-login/main-login.component.ts
@@ -3,13 +3,14 @@ import { RegisterComponent } from "./register/register.component";
 import { ChooseAvatarComponent } from "./choose-avatar/choose-avatar.component";
 import { LoginComponent } from "./login/login.component";
 import { PasswordForgetComponent } from "./password-forget/password-forget.component";
+import { PasswordResetComponent } from "./password-reset/password-reset.component";
 
 @Component({
     selector: 'app-main-login',
     standalone: true,
     templateUrl: './main-login.component.html',
     styleUrl: './main-login.component.scss',
-    imports: [RegisterComponent, ChooseAvatarComponent, LoginComponent, PasswordForgetComponent]
+    imports: [RegisterComponent, ChooseAvatarComponent, LoginComponent, PasswordForgetComponent, PasswordResetComponent]
 })
 export class MainLoginComponent {
   
--- src/app/components/main-login/password-reset/password-reset.component.ts
+++ src/app/components/main-login/password-reset/password-reset.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../../../shared/components/login/header/header.component';
+import { SmallBtnComponent } from "../../../shared/components/small-btn/small-btn.component";
+
 
 @Component({
   selector: 'app-password-reset',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, HeaderComponent, FooterComponent, RouterModule, SmallBtnComponent],
   templateUrl: './password-reset.component.html',
   styleUrl: './password-reset.component.scss'
 })
 export class PasswordResetComponent {
+  password: string = '';
+  passwordRepeat: string = '';
+
+  onSubmit(ngForm: NgForm) {
+    console.log('LogingVersuch mit:', this.password);
+   
+    // ngForm.resetForm();
+  }
+  passwordsMatch(): boolean {
+    return this.password === this.passwordRepeat;
+  }
 
 }
