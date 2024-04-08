8600aa5893c1d3bf936be04e3facbfc5215ca973
HomeBjo <bjoernteneicken@gmail.com>
2024-04-08 11:51:33 +0200

    add login html/scss

--- src/app/components/main-login/login/login.component.ts
+++ src/app/components/main-login/login/login.component.ts
@@ -1,28 +1,38 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { loginService } from '../../../service/login.service';
 import { Firestore } from '@angular/fire/firestore';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../../../shared/components/login/header/header.component";
+import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [FormsModule,CommonModule, HeaderComponent, FooterComponent,RouterModule]
 })
 export class LoginComponent {
   firestore: Firestore = inject(Firestore);
   email: string = '';
   password: string = '';
-  name: string = '';
 
   constructor() {}
+
   onSubmit(ngForm: NgForm) {
     console.log('LogingVersuch mit:', this.email, this.password);
     this.login();
     // ngForm.resetForm();
   }
+
+  formGroup = new FormGroup({
+    nameField: new FormControl('', Validators.required),
+    emailField: new FormControl('', [Validators.required, Validators.email]),
+    passwordField: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
   login() {
     const auth = getAuth();
@@ -31,8 +41,7 @@ export class LoginComponent {
       console.info('E-Mail und Passwort dürfen nicht leer sein.');
       return; // wens Validiert ist
     }
-    
-   
+
     signInWithEmailAndPassword(auth, this.email, this.password)
       .then((userCredential) => {
         // eingeloggt
