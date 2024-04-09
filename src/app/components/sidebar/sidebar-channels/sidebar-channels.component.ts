1d3fc53c67f3c56fc2bf92967ed1900cc3e22881
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-04-09 12:41:28 +0200

    changed some style things

--- src/app/components/sidebar/sidebar-channels/sidebar-channels.component.ts
+++ src/app/components/sidebar/sidebar-channels/sidebar-channels.component.ts
@@ -4,24 +4,31 @@ import { Channel } from '../../../interface/channel.interface';
 import { RouterLink } from '@angular/router';
 import { ChatService } from '../../../service/chat.service';
 import { SmallBtnComponent } from '../../../shared/components/small-btn/small-btn.component';
+import { CommonModule } from '@angular/common';
+import { AddNewChannelComponent } from './add-new-channel/add-new-channel.component';
 
 @Component({
   selector: 'app-sidebar-channels',
   standalone: true,
-  imports: [RouterLink, SmallBtnComponent],
+  imports: [RouterLink, SmallBtnComponent, CommonModule, AddNewChannelComponent],
   templateUrl: './sidebar-channels.component.html',
   styleUrl: './sidebar-channels.component.scss',
 })
 export class SidebarChannelsComponent {
+
   constructor(
     public channelService: ChannleService,
     public chatService: ChatService
   ) {}
 
+
   closeSecondaryChat() {
     this.chatService.isSecondaryChatId = '';
   }
+
+
   getChannels(): Channel[] {
     return this.channelService.allChannels;
   }
+
 }
