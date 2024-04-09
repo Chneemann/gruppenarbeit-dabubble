ed45f113effd231844f24fac879c8c310f289be7
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-04-09 14:26:05 +0200

    added new add channel comp

--- src/app/components/sidebar/sidebar-channels/add-new-channel/add-new-channel.component.ts
+++ src/app/components/sidebar/sidebar-channels/add-new-channel/add-new-channel.component.ts
@@ -1,22 +1,35 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ChannleService } from '../../../../service/channle.service';
+import { SmallBtnComponent } from '../../../../shared/components/small-btn/small-btn.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-channel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SmallBtnComponent, FormsModule],
   templateUrl: './add-new-channel.component.html',
   styleUrl: './add-new-channel.component.scss'
 })
 export class AddNewChannelComponent {
 
+  channelName: string = '';
+  channelDescription: string = '';
+  shwoNextWindow: boolean = false;
+
+
   constructor(public channelServide: ChannleService){}
 
 
   toggleShowAddChannelBox(){
-    console.log('---------------');
-    
     this.channelServide.showAddChannelBox = !this.channelServide.showAddChannelBox;
+    this.shwoNextWindow = false;
+  }
+
+
+  createNewChannel(){
+    this.shwoNextWindow = !this.shwoNextWindow;
+    console.log('channelName:', this.channelName);
+    console.log('description:', this.channelDescription);
   }
 }
