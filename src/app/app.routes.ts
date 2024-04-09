0dd9f5a6d465ee1ecdd7fa7f3f60b1ebf3cbad60
HomeBjo <bjoernteneicken@gmail.com>
2024-04-09 14:13:22 +0200

    set routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { MainLoginComponent } from './components/main-login/main-login.component';
 import { RegisterComponent } from './components/main-login/register/register.component';
 import { ChooseAvatarComponent } from './components/main-login/choose-avatar/choose-avatar.component';
+import { PasswordForgetComponent } from './components/main-login/password-forget/password-forget.component';
+import { PasswordResetComponent } from './components/main-login/password-reset/password-reset.component';
+import { LoginComponent } from './components/main-login/login/login.component';
 
 export const routes: Routes = [
    { path: '', component: MainComponent },
-  { path: 'login', component: MainLoginComponent },
+  { path: 'login', component: LoginComponent },
    { path: 'register', component: RegisterComponent },
-  // { path: 'Datenschutz', component: ChooseAvatarComponent },
+   { path: 'avatar', component: ChooseAvatarComponent },
+   { path: 'passwordForget', component: PasswordForgetComponent },
+   { path: 'passwordReset', component: PasswordResetComponent },
   { path: 'main', component: MainComponent },
   { path: 'main/:id', component: MainComponent },
 ];
