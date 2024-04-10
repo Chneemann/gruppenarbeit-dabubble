a64b51685ff4274dce5e096c25a06aed13de4fe4
HomeBjo <bjoernteneicken@gmail.com>
2024-04-10 09:04:12 +0200

    login.ts Import to loginservice.ts

--- src/app/components/main-login/login/login.component.ts
+++ src/app/components/main-login/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { loginService } from '../../../service/login.service';
-import { Firestore } from '@angular/fire/firestore';
+
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { HeaderComponent } from "../../../shared/components/login/header/header.component";
 import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
 import { CommonModule } from '@angular/common';
@@ -16,60 +16,16 @@ import { CommonModule } from '@angular/common';
     imports: [FormsModule,CommonModule, HeaderComponent, FooterComponent,RouterLink]
 })
 export class LoginComponent {
-  firestore: Firestore = inject(Firestore);
-  email: string = '';
-  password: string = '';
 
-  constructor(private loginService: loginService) {}
+
+
+  constructor(public loginService: loginService) {}
 
   onSubmit(ngForm: NgForm) {
-    console.log('LogingVersuch mit:', this.email, this.password);
-    this.login();
+    console.log('LogingVersuch mit:', this.loginService.email, this.loginService.password);
+    this.loginService.login();
     // ngForm.resetForm();
   }
 
-  formGroup = new FormGroup({
-    nameField: new FormControl('', Validators.required),
-    emailField: new FormControl('', [Validators.required, Validators.email]),
-    passwordField: new FormControl('', [Validators.required, Validators.minLength(6)])
-  });
-
-  login() {
-    const auth = getAuth();
-    
-    if (!this.email || !this.password) {
-      console.info('E-Mail und Passwort dürfen nicht leer sein.');
-      return; // wens Validiert ist
-    }
-
-    signInWithEmailAndPassword(auth, this.email, this.password)
-      .then((userCredential) => {
-        // eingeloggt
-        const user = userCredential.user;
-        console.log('Eingeloggt als:', user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-  
-        // Fehlerbehandlung bei aktuellem Fehlercode
-        switch (errorCode) {
-          case 'auth/user-not-found':
-            console.info('Kein Benutzer mit dieser E-Mail gefunden.');
-            break;
-          case 'auth/wrong-password':
-            console.info('Falsches Passwort.');
-            break;
-          case 'auth/invalid-email':
-            console.info('Ungültiges E-Mail-Format.');
-            break;
-          case 'auth/invalid-credential':
-            console.info('Ungültige Anmeldeinformationen. Bitte überprüfen Sie Ihre Eingaben.');
-            break;
-          default:
-            console.info('Login-Fehler:', errorMessage);
-        }
-      });
-  }
     }
 
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,18 +1,63 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject,Component } from '@angular/core';
 import {
   Firestore,
   addDoc,
   collection,
 } from '@angular/fire/firestore';
 import { User } from '../interface/user.interface';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { FormControl, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class loginService {
   firestore: Firestore = inject(Firestore);
-  constructor() {
+  email: string = '';
+  password: string = '';
+  constructor( public router: Router) {
 
+  }
+
+  login() {
+    const auth = getAuth();
+    
+    if (!this.email || !this.password) {
+      console.info('E-Mail und Passwort dürfen nicht leer sein.');
+      return; // wens Validiert ist
+    }
+
+    signInWithEmailAndPassword(auth, this.email, this.password)
+      .then((userCredential) => {
+        // eingeloggt
+        const user = userCredential.user;
+        console.log('Eingeloggt als:', user);
+        this.router.navigate(['/main']);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+  
+        // Fehlerbehandlung bei aktuellem Fehlercode
+        switch (errorCode) {
+          case 'auth/user-not-found':
+            console.info('Kein Benutzer mit dieser E-Mail gefunden.');
+            break;
+          case 'auth/wrong-password':
+            console.info('Falsches Passwort.');
+            break;
+          case 'auth/invalid-email':
+            console.info('Ungültiges E-Mail-Format.');
+            break;
+          case 'auth/invalid-credential':
+            console.info('Ungültige Anmeldeinformationen. Bitte überprüfen Sie Ihre Eingaben.');
+            break;
+          default:
+            console.info('Login-Fehler:', errorMessage);
+        }
+      });
   }
 
 
