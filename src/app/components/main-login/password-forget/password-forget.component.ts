c384aa826f87c7933e5d66899e21a5303d27e4df
HomeBjo <bjoernteneicken@gmail.com>
2024-04-10 10:37:50 +0200

    add forgetPassword email sending

--- src/app/components/main-login/password-forget/password-forget.component.ts
+++ src/app/components/main-login/password-forget/password-forget.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { FooterComponent } from "../../../shared/components/login/footer/footer.component";
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../../../shared/components/login/header/header.component';
 import { SmallBtnComponent } from "../../../shared/components/small-btn/small-btn.component";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { Firestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-password-forget',
@@ -15,10 +18,28 @@ import { SmallBtnComponent } from "../../../shared/components/small-btn/small-bt
 })
 export class PasswordForgetComponent {
   email: string = '';
+  firestore: Firestore = inject(Firestore);
+  constructor(private router: Router){}
+
+  passwordReset(){
+    const auth = getAuth();
+    sendPasswordResetEmail(auth, this.email)
+     .then(() => {
+       // Password reset email sent!
+       console.log('test hier',this.email)
+        // this.router.navigate(['/login']);  später zum weiter leiten 
+     })
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       // ..
+     });
+  }
+
 
   onSubmit(ngForm: NgForm) {
     console.log('LogingVersuch mit:', this.email);
-   
+   this.passwordReset()
     // ngForm.resetForm();
   }
 }
