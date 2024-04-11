205c99500ba7adc5b0bbb6eaf10940f9f9073c99
Chneemann <Andre-Ke@gmx.de>
2024-04-11 08:57:52 +0200

    design menu

--- src/app/components/main-chat/single-chat/options-menu/options-menu.component.ts
+++ src/app/components/main-chat/single-chat/options-menu/options-menu.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { SingleChatComponent } from '../single-chat.component';
+import { SmallBtnComponent } from '../../../../shared/components/small-btn/small-btn.component';
 
 @Component({
   selector: 'app-options-menu',
   standalone: true,
-  imports: [CommonModule, SingleChatComponent],
+  imports: [CommonModule, SingleChatComponent, SmallBtnComponent],
   templateUrl: './options-menu.component.html',
   styleUrl: './options-menu.component.scss',
 })
