f6bdb0c96b5cad30f1440c584b39f048371f1cda
Chneemann <Andre-Ke@gmx.de>
2024-04-15 14:56:18 +0200

    added reaction dialog

--- src/app/components/main-chat/single-chat/reaction-emojis/reaction-emojis.component.ts
+++ src/app/components/main-chat/single-chat/reaction-emojis/reaction-emojis.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { SmallBtnComponent } from '../../../../shared/components/small-btn/small-btn.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reaction-emojis',
   standalone: true,
-  imports: [SmallBtnComponent],
+  imports: [CommonModule, SmallBtnComponent],
   templateUrl: './reaction-emojis.component.html',
   styleUrl: './reaction-emojis.component.scss',
 })
-export class ReactionEmojisComponent {}
+export class ReactionEmojisComponent {
+  isDialogOpen: boolean = false;
+
+  toggleDialog() {
+    this.isDialogOpen = !this.isDialogOpen;
+  }
+}
