dbe9c1d735114daa2d5a8b8f7eb3c51b61b67c84
HomeBjo <bjoernteneicken@gmail.com>
2024-04-16 18:37:09 +0200

    style imprint and police

--- src/app/shared/components/imprint/imprint.component.ts
+++ src/app/shared/components/imprint/imprint.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from "../login/header/header.component";
+import { SmallBtnComponent } from "../small-btn/small-btn.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-imprint',
-  standalone: true,
-  imports: [TranslateModule],
-  templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss',
+    selector: 'app-imprint',
+    standalone: true,
+    templateUrl: './imprint.component.html',
+    styleUrl: './imprint.component.scss',
+    imports: [TranslateModule, HeaderComponent, SmallBtnComponent,RouterLink]
 })
 export class ImprintComponent {
   constructor(private location: Location) {}
--- src/app/shared/components/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/components/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from "../login/header/header.component";
+import { SmallBtnComponent } from "../small-btn/small-btn.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-privacy-policy',
-  standalone: true,
-  imports: [TranslateModule],
-  templateUrl: './privacy-policy.component.html',
-  styleUrl: './privacy-policy.component.scss',
+    selector: 'app-privacy-policy',
+    standalone: true,
+    templateUrl: './privacy-policy.component.html',
+    styleUrl: './privacy-policy.component.scss',
+    imports: [TranslateModule, HeaderComponent, SmallBtnComponent, RouterLink]
 })
 export class PrivacyPolicyComponent {
   constructor(private location: Location) {}
