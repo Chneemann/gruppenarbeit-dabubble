1e273c20006ec39c7b515596e1228e773fae75f1
HomeBjo <bjoernteneicken@gmail.com>
2024-04-29 11:16:21 +0200

    jsdoc language.service

--- src/app/service/language.service.ts
+++ src/app/service/language.service.ts
@@ -11,6 +11,14 @@ export class LanguageService {
     this.translate.setDefaultLang(this.currentLang);
   }
 
+
+  /**
+ * Switches the application's current language.
+ * 
+ * This method sets the current language based on the specified `lang` parameter and updates the translation service to use the new language. This is useful for enabling runtime language switching in a multi-lingual application.
+ * 
+ * @param lang The language code to switch to. Supported values are 'en' for English and 'de' for German.
+ */
   switchLanguage(lang: string) {
     if (lang === 'en') {
       this.currentLang = 'en';
